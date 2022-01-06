2fd1b5effb2cf17b0fe9fb5fca5925a348c1915d
madnessTroy <huutoanchu@gmail.com>
2022-01-06 11:32:58 +0700

    LAB_1.7

--- app.js
+++ app.js
@@ -1,57 +1,10 @@
 // Core module
 const http = require("http");
-const fs = require("fs"); // file system
-const { builtinModules } = require("module");
 
-const server = http.createServer((req, res) => {
-	console.log(req);
-	// process.exit() -- kết thúc vòng đời của node nếu không có res nào trả đi từ server
-	const url = req.url;
-	const method = req.method;
+// custom module
+const routes = require("./routes");
 
-	// Path "/"
-	if (url === "/") {
-		res.write("<html>");
-		res.write(`
-            <head>
-                <title>Enter message</title>
-            </head>
-        `);
-		res.write(`
-		    <body>
-		        <form method="POST" action="/message">
-		            <input type="text" name="message">
-		            <button type="submit">Send</button>
-		        </form>
-		    </body>
-		`);
-		res.write("</html>");
-		return res.end();
-	}
-
-	// Path "/message"
-	if (url === "/message" && method === "POST") {
-		const body = [];
-		req.on("data", (chunk) => {
-			body.push(chunk);
-		});
-		req.on("end", () => {
-			const parsedBody = Buffer.concat(body).toString();
-			const message = parsedBody.split("=")[1];
-			fs.writeFileSync("message.txt", message); // tạo 1 file tên "message.txt" có nội dung DUMMY
-		});
-		res.statusCode = 302; // status found
-		res.setHeader("Location", "/"); // relocate về path "/"
-		return res.end();
-	}
-
-	// Tạo res cho phía client
-	res.setHeader("Content-Type", "text/html"); // Đặt header cho res (HTML)
-	res.write("<html>");
-	res.write("<head><title>My first NodeJS page</title></head>");
-	res.write("<h1>Hello from my Node.js server!</h1>");
-	res.write("</html>");
-	res.end(); // lưu ý phải có dòng này, sau dòng này thì sẽ không có res nào được gửi đi
-});
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(3000); // Tạo web server ở port 3000
---
+++ routes.js
@@ -0,0 +1,55 @@
+const fs = require("fs"); // file system
+
+const requestHandler = (req, res) => {
+	const url = req.url;
+	const method = req.method;
+	// Path "/"
+	if (url === "/") {
+		res.write("<html>");
+		res.write(`
+            <head>
+                <title>Enter message</title>
+            </head>
+        `);
+		res.write(`
+		    <body>
+		        <form method="POST" action="/message">
+		            <input type="text" name="message">
+		            <button type="submit">Send</button>
+		        </form>
+		    </body>
+		`);
+		res.write("</html>");
+		return res.end();
+	}
+
+	// Path "/message"
+	if (url === "/message" && method === "POST") {
+		const body = [];
+		req.on("data", (chunk) => {
+			body.push(chunk);
+		});
+		req.on("end", () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split("=")[1];
+			fs.writeFile("message.txt", message, (err) => {
+				res.statusCode = 302; // status found
+				res.setHeader("Location", "/"); // relocate về path "/"
+				return res.end();
+			});
+		});
+	}
+
+	// Tạo res cho phía client
+	res.setHeader("Content-Type", "text/html"); // Đặt header cho res (HTML)
+	res.write("<html>");
+	res.write("<head><title>My first NodeJS page</title></head>");
+	res.write("<h1>Hello from my Node.js server!</h1>");
+	res.write("</html>");
+	res.end(); // lưu ý phải có dòng này, sau dòng này thì sẽ không có res nào được gửi đi
+};
+
+module.exports = {
+	handler: requestHandler,
+	someText: "Hello",
+};
