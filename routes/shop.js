1cf003d99fb48e437d47136e4f3546b0a390a892
madnessTroy <huutoanchu@gmail.com>
2022-01-08 14:34:48 +0700

    LAB_2.13

--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,11 @@ const path = require("path");
 const express = require("express");
 // Router
 const router = express.Router();
+//
+const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-	res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+	res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
