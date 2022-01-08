d0beb589d6f505933633832c1221f52e620014cf
madnessTroy <huutoanchu@gmail.com>
2022-01-08 13:15:21 +0700

    LAB_2.5

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 // ExpressJS
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 // Tạo middleware
-app.use("/", (req, res, next) => {
-	console.log("This always runs");
-	next();
-});
-
 app.use("/add-product", (req, res, next) => {
-	res.send("<h1>The Add product page</h1>");
+	res.send(`
+        <form method="POST" action="/product">
+            <input type="text" name="title" />
+            <button type="submit">Add product</button>
+        </form>
+    `);
+});
+
+app.use("/product", (req, res, next) => {
+	console.log(req.body);
+	res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-	console.log("Hello from second middleware");
 	res.send("<h1>Hello fromm Express!</h1>"); // Mặc định res.setHeader của express là 'text/html'
 });
 
