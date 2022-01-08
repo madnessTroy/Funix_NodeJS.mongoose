c5a26151667a7f5e701c0f98fc2a102edad29d00
madnessTroy <huutoanchu@gmail.com>
2022-01-08 12:28:33 +0700

    LAB_2.3

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const app = express();
 // Tạo middleware
 app.use((req, res, next) => {
 	console.log("Hello from first middleware");
-	next();
+	next(); // Cho phép thực hiên middleware kế tiếp
 });
 
 app.use((req, res, next) => {
 	console.log("Hello from second middleware");
+	res.send("<h1>Hello fromm Express!</h1>"); // Mặc định res.setHeader của express là 'text/html'
 });
+
 // app sẽ nắm giữ các logic cho server (req,res) được tạo bởi express
 const server = http.createServer(app);
 
