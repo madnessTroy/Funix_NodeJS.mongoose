a6f70c41b334b1fec6eea61b686fac249d90c8d2
madnessTroy <huutoanchu@gmail.com>
2022-01-08 12:22:50 +0700

    LAB_2.2

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Core module
 const http = require("http");
 
-// custom module
-const routes = require("./routes");
+// ExpressJS
+const express = require("express");
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
+
+// Tạo middleware
+app.use((req, res, next) => {
+	console.log("Hello from first middleware");
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log("Hello from second middleware");
+});
+// app sẽ nắm giữ các logic cho server (req,res) được tạo bởi express
+const server = http.createServer(app);
 
 server.listen(3000); // Tạo web server ở port 3000
