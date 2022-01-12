3d375b7fc1e6e46c7cc9be7050e5b55c3fcefc69
madnessTroy <huutoanchu@gmail.com>
2022-01-12 20:10:48 +0700

    LAB_5.7

--- models/product.js
+++ models/product.js
@@ -20,9 +20,15 @@ module.exports = class Product {
 
 	static deleteById(id) {}
 
-	static fetchAll(cb) {
+	static fetchAll() {
 		return db.execute("SELECT * FROM products");
 	}
 
-	static findById(id, cb) {}
+	static findById(id) {
+		return db.execute(
+			`SELECT * FROM products
+             WHERE products.id = ?`,
+			[id]
+		);
+	}
 };
