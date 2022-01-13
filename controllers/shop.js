b20edb927573da956c4841f64caec332ea1e1264
madnessTroy <huutoanchu@gmail.com>
2022-01-13 09:23:47 +0700

    LAB_5.13

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,8 +20,8 @@ exports.getProducts = (req, res, next) => {
 // Product detail
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
-	Product.findById(prodId)
-		.then(([product]) => {
+	Product.findAll({ where: { id: prodId } })
+		.then((product) => {
 			res.render("shop/product-detail", {
 				product: product[0],
 				pageTitle: product.title,
@@ -31,6 +31,17 @@ exports.getProduct = (req, res, next) => {
 		.catch((err) => {
 			console.log(err);
 		});
+	// Product.findByPk(prodId)
+	// 	.then((product) => {
+	// 		res.render("shop/product-detail", {
+	// 			product: product,
+	// 			pageTitle: product.title,
+	// 			path: "/products",
+	// 		});
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	});
 };
 
 //
