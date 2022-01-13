61a97ed1c98409092c4e71c959c258970c65b745
madnessTroy <huutoanchu@gmail.com>
2022-01-13 21:34:39 +0700

    LAB_5.21

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const sequelize = require("./util/database");
 
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -34,12 +36,17 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// Relations
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-	//.sync({ force: true })
-	.sync()
+	.sync({ force: true })
+	//.sync()
 	.then((result) => {
 		return User.findByPk(1);
 		// console.log(result);
