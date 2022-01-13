c90ca00d37c5e7cf97abe4a05259923ec0d5f383
madnessTroy <huutoanchu@gmail.com>
2022-01-13 22:01:04 +0700

    LAB_5.22

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-	.sync({ force: true })
-	//.sync()
+	//.sync({ force: true })
+	.sync()
 	.then((result) => {
 		return User.findByPk(1);
 		// console.log(result);
@@ -58,6 +58,9 @@ sequelize
 		return user;
 	})
 	.then((user) => {
+		return user.createCart();
+	})
+	.then((cart) => {
 		app.listen(3000);
 	})
 	.catch((err) => {
