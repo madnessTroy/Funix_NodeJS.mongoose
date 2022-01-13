54e6d74d6e13be96c25165d9e6e29d1a15b1d652
madnessTroy <huutoanchu@gmail.com>
2022-01-13 22:13:27 +0700

    LAB_5.23

--- controllers/shop.js
+++ controllers/shop.js
@@ -80,6 +80,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
 	let fetchedCart;
+	let newQuantity = 1;
 	req.user
 		.getCart()
 		.then((cart) => {
@@ -91,12 +92,15 @@ exports.postCart = (req, res, next) => {
 			if (products.length > 0) {
 				product = products[0];
 			}
-			let newQuantity = 1;
-			return Product.findByPk(prodId)
-				.then((product) => {
-					return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
-				})
-				.catch((err) => console.log(err));
+			if (product) {
+				const oldQuantity = product.cartItem.quantity;
+				newQuantity = oldQuantity + 1;
+				return product;
+			}
+			return Product.findByPk(prodId);
+		})
+		.then((product) => {
+			return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
 		})
 		.then(() => res.redirect("/cart"))
 		.catch((err) => console.log(err));
