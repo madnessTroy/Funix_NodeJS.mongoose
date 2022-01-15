d2623d50b59a48eb8297cd166f7762f18aec5a13
madnessTroy <huutoanchu@gmail.com>
2022-01-15 08:09:38 +0700

    LAB_5.26

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const errorController = require("./controllers/error");
 const app = express();
 const sequelize = require("./util/database");
 
+// Database tables
 const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -43,10 +46,11 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-	//.sync({ force: true })
-	.sync()
+	.sync({ force: true })
+	// .sync()
 	.then((result) => {
 		return User.findByPk(1);
 		// console.log(result);
