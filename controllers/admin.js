c8aac8fb8e5456e6c9c652cfb5d1d3ecf350cbc3
madnessTroy <huutoanchu@gmail.com>
2022-01-16 12:16:21 +0700

    LAB 6.11

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require("mongodb");
 const Product = require("../models/product");
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product", {
@@ -56,13 +53,7 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedImageUrl = req.body.imageUrl;
 	const updatedPrice = req.body.price;
 	const updatedDesc = req.body.description;
-	const product = new Product(
-		updatedTitle,
-		updatedPrice,
-		updatedDesc,
-		updatedImageUrl,
-		new ObjectId(prodId)
-	);
+	const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
 	product
 		.save()
 		.then(() => {
