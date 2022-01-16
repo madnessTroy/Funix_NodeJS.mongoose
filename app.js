c527b246a4233229b590282d2730c952c0bea9d9
madnessTroy <huutoanchu@gmail.com>
2022-01-16 09:26:13 +0700

    LAB 6.2__6.3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const mongoConnect = require("./util/database");
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// const adminRoutes = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 // const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {});
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
--- util/database.js
+++ util/database.js
@@ -7,7 +7,7 @@ const url =
 const mongoConnect = (callback) => {
 	MongoClient.connect(url)
 		.then((client) => {
-			console.log(client);
+			console.log("Connected");
 			callback(client);
 		})
 		.catch((err) => console.log(err));
