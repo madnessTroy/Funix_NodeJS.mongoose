83cb8d2fb4a70d6a58ca852c9e4f89e58e2d5bb3
madnessTroy <huutoanchu@gmail.com>
2022-01-16 14:33:26 +0700

    LAB 6.14

--- models/user.js
+++ models/user.js
@@ -3,14 +3,27 @@ const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 class User {
-	constructor(username, email) {
+	constructor(username, email, cart, id) {
 		this.name = username;
 		this.email = email;
+		this.cart = cart;
+		this.id = _id;
 	}
 
 	save() {
 		const db = getDb();
 		return db.collection('users').insertOne(this);
+	}
+
+	addToCart(product) {
+		const updatedCart = { items: [{ ...product, quantity: 1 }] };
+		const db = getDb();
+		return db
+			.collection('users')
+			.updateOne(
+				{ _id: new ObjectId(this._id) },
+				{ $set: { cart: updatedCart } }
+			);
 	}
 
 	static findById(userId) {
