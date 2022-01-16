dc86b7cab9ffdd3e96621be7fb03a68f3ebcd9c7
madnessTroy <huutoanchu@gmail.com>
2022-01-16 09:50:41 +0700

    LAB 6.4

--- models/product.js
+++ models/product.js
@@ -1,9 +1,11 @@
+const getDb = require("../util/database").getDb
+
 class Product {
 	constructor(title, price, description, imageUrl ) {
-		this.title: title,
-		this.price: price,
-		this.description: description,
-		this.imageUrl: imageUrl
+		this.title = title,
+		this.price = price,
+		this.description = description,
+		this.imageUrl = imageUrl
 	}
 
 	save()
