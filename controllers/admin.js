bd274bc2d851ad47c45899949a8c62bf1dc6f7b2
madnessTroy <huutoanchu@gmail.com>
2022-01-17 19:40:56 +0700

    LAB_7.6

--- controllers/admin.js
+++ controllers/admin.js
@@ -57,15 +57,14 @@ exports.postEditProduct = (req, res, next) => {
 	const updatedImageUrl = req.body.imageUrl;
 	const updatedPrice = req.body.price;
 	const updatedDesc = req.body.description;
-	const product = new Product(
-		updatedTitle,
-		updatedPrice,
-		updatedDesc,
-		updatedImageUrl,
-		prodId
-	);
-	product
-		.save()
+	Product.findById(prodId)
+		.then((product) => {
+			product.title = updatedTitle;
+			product.price = updatedPrice;
+			product.description = updatedDesc;
+			product.imageUrl = updatedImageUrl;
+			return product.save();
+		})
 		.then(() => {
 			console.log('UPDATED DATA SUCCESS!');
 			res.redirect('/admin/products');
@@ -74,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-	Product.fetchAll()
+	Product.find()
 		.then((products) => {
 			res.render('admin/products', {
 				prods: products,
