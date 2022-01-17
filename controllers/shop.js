13113a06e70bd0761028c585294128085e8ab8e1
madnessTroy <huutoanchu@gmail.com>
2022-01-17 22:53:03 +0700

    LAB_7.14

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
 	Product.find()
@@ -46,7 +47,7 @@ exports.getCart = (req, res, next) => {
 	req.user
 		.populate(['cart.items.productId'])
 		.then((user) => {
-			console.log(user.cart.items);
+			//console.log(user.cart.items);
 			const products = user.cart.items;
 			res.render('shop/cart', {
 				path: '/cart',
@@ -81,7 +82,20 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
 	req.user
-		.addOrder()
+		.populate(['cart.items.productId'])
+		.then((user) => {
+			const products = user.cart.items.map((item) => {
+				return { productId: item.productId, quantity: item.quantity };
+			});
+			const order = new Order({
+				products: products,
+				user: {
+					name: req.user.name,
+					userId: req.user,
+				},
+			});
+			return order.save();
+		})
 		.then((result) => {
 			res.redirect('/orders');
 		})
